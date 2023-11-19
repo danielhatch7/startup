889f1c7867533c9c5b15f99785e1ad97feb11d54
Daniel Hatch <danielhatch7@gmail.com>
2023-11-18 19:02:11 -0700

    adding mongodb support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const DB = require("./database.js");
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -15,42 +16,109 @@ const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 // GetResults TODO: FINISH
-apiRouter.get("/results/*", (_req, res) => {
+apiRouter.get("/results/*", async (_req, res) => {
   let sessionID = getSessionID(_req.originalUrl);
-  let session_results = getResults(sessionID);
-  res.send(session_results);
+  let session_results = await DB.getResults(sessionID);
+  let results = [];
+  if (session_results.length == 0) {
+    results = [0, 0, 0, 0];
+  } else {
+    let results_object = session_results[0];
+    results = results_object.results;
+  }
+  res.send(results);
 });
 
 // GetInfo TODO: FINISH
-apiRouter.get("/info/*", (_req, res) => {
+apiRouter.get("/info/*", async (_req, res) => {
   let sessionID = getSessionID(_req.originalUrl);
-  let session_info = getInfo(sessionID);
-  res.send(session_info);
+  let session_data = await DB.getInfo(sessionID);
+  let session_info = session_data[0];
+  let info = [
+    session_info.question,
+    session_info.response1,
+    session_info.response2,
+    session_info.response3,
+    session_info.response4,
+    session_info.is_live,
+  ];
+
+  res.send(info);
 });
 
 // GetValid TODO: FINISH
-apiRouter.post("/valid/*", (req, res) => {
+apiRouter.post("/valid/*", async (req, res) => {
   let sessionID = getSessionID(req.originalUrl);
-  valid = getValid(req.body, valid);
+  let valid = await DB.getValid(sessionID);
 
-  res.send(valid);
+  let body = req.body;
+
+  if (valid.length == 0) {
+    body.message = "Invalid SessionID. Please try again.";
+  } else {
+    body.message = "";
+    body.valid = true;
+  }
+  let valid_result = [];
+  valid_result.push(body);
+
+  res.send(valid_result);
 });
 
 // StartSession TODO: FINISH
-apiRouter.put("/start", (req, res) => {
-  startSession(req.body);
+apiRouter.put("/start", async (req, res) => {
+  await DB.startSession(req.body);
 });
 
 // StartSession TODO: FINISH
-apiRouter.put("/end", (req, res) => {
-  endSession(req.body);
-  res.send(valid);
+apiRouter.put("/end", async (req, res) => {
+  await DB.endSession(req.body);
 });
 
 // SubmitVote TODO: FINISH
-apiRouter.post("/vote/*", (req, res) => {
-  session_results = updateVote(req.body);
-  res.send(session_results);
+apiRouter.post("/vote/*", async (req, res) => {
+  let sessionID = getSessionID(req.originalUrl);
+  let ballots_info = await DB.getVote(sessionID);
+  let ballots_object = ballots_info[0];
+  let old_ballots = ballots_object.voters;
+  let results_info = await DB.getResults(sessionID);
+  let results_object = results_info[0];
+  let old_results = results_object.results;
+
+  let username = req.body.userName;
+  let vote = req.body.vote;
+  let vote_index = parseInt(vote[vote.length - 1]);
+
+  let ballots = old_ballots;
+  let old_vote = 4;
+  let ballot_index = ballots.length;
+
+  for (let i = 0; i < ballots.length; i++) {
+    if (ballots[i].userName === username) {
+      old_vote = ballots[i].vote;
+      ballot_index = i;
+    }
+  }
+
+  if (old_vote != 4) {
+    old_results[old_vote] -= 1;
+  }
+
+  old_results[vote_index] += 1;
+
+  if (ballot_index === ballots.length) {
+    ballots.push({ userName: username, vote: vote_index });
+  } else {
+    ballots[ballot_index] = { userName: username, vote: vote_index };
+  }
+
+  await DB.updateVote(sessionID, ballots);
+  await DB.updateResults(sessionID, old_results);
+  let new_results_info = await DB.getResults(sessionID);
+  let new_results_object = new_results_info[0];
+  let new_results = new_results_object.results;
+
+  res.send(new_results);
 });
 
 // Return the application's default page if the path is unknown
@@ -62,115 +130,9 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// The results and info are saved in memory and disappear whenever the service is restarted.
-let results = new Map();
-let info = new Map();
-let voters = new Map();
-let valid = [];
-
 // TODO: FINISH
 function getSessionID(URL) {
   let pieces = URL.split("/");
   let sessionID = pieces[pieces.length - 1];
   return sessionID;
-}
-
-// TODO: FINISH
-function startSession(session_info) {
-  let sessionID = session_info.sessionID;
-  let question = session_info.question;
-  let response1 = session_info.response1;
-  let response2 = session_info.response2;
-  let response3 = session_info.response3;
-  let response4 = session_info.response4;
-  let is_live = session_info.is_live;
-
-  info.set(sessionID, [
-    question,
-    response1,
-    response2,
-    response3,
-    response4,
-    is_live,
-  ]);
-  results.set(sessionID, [0, 0, 0, 0]);
-  voters.set(sessionID, []);
-}
-
-function endSession(session_info) {
-  let sessionID = session_info.sessionID;
-  let is_live = session_info.is_live;
-
-  let current = info.get(sessionID);
-  current[current.length - 1] = is_live;
-
-  info.set(sessionID, current);
-}
-
-// TODO: FINISH
-function getResults(sessionID) {
-  let temp = results.get(sessionID);
-  return temp;
-}
-
-// TODO: FINISH
-function getInfo(sessionID) {
-  let temp = info.get(sessionID);
-  return temp;
-}
-
-// TODO: FINISH
-function getValid(body, valid) {
-  valid.length = 0;
-  let sessionID = body.sessionID;
-  let temp = info.has(sessionID);
-
-  body.valid = temp;
-
-  if (temp) {
-    body.message = "";
-  } else {
-    body.message = "Invalid SessionID. Please try again.";
-  }
-  valid.push(body);
-
-  return valid;
-}
-
-// TODO: FINISH
-function updateVote(vote_info) {
-  let sessionID = vote_info.sessionID;
-  let username = vote_info.userName;
-  let vote = vote_info.vote;
-  let vote_index = parseInt(vote[vote.length - 1]);
-
-  // FINISH
-  let ballots = voters.get(sessionID);
-  let old_vote = 4;
-  let ballot_index = ballots.length;
-
-  for (let i = 0; i < ballots.length; i++) {
-    if (ballots[i].userName === username) {
-      old_vote = ballots[i].vote;
-      ballot_index = i;
-    }
-  }
-
-  let temp = results.get(sessionID);
-
-  if (old_vote != 4) {
-    temp[old_vote] -= 1;
-  }
-
-  if (ballot_index === ballots.length) {
-    ballots.push({ userName: username, vote: vote_index });
-  } else {
-    ballots[ballot_index] = { userName: username, vote: vote_index };
-  }
-
-  temp[vote_index] += 1;
-
-  voters.set(sessionID, ballots);
-  results.set(sessionID, temp);
-  return temp;
 }
